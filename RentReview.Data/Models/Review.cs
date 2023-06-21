300af1210b132cb08182c410f66b72bb152257fb
svilen iordanov <svilqkaiordanov@gmail.com>
2023-06-21 15:39:24 +0300

    Review model

---
+++ RentReview.Data/Models/Review.cs
@@ -0,0 +1,9 @@
+﻿namespace RentReview.Data.Models
+{
+    public class Review
+    {
+        public string Id = Guid.NewGuid().ToString();
+        public string TenantId { get; set; }
+        public string PropertyId { get; set; }
+    }
+}
