1310f47c341c0c9e485ea0d5941826e631bece03
svilen iordanov <svilqkaiordanov@gmail.com>
2023-06-21 15:35:54 +0300

    Tenant model

---
+++ RentReview.Data/Models/Tenant.cs
@@ -0,0 +1,12 @@
+﻿namespace RentReview.Data.Models
+{
+    public class Tenant
+    {
+        public string Id = Guid.NewGuid().ToString();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName { get { return this.FirstName + " " + this.LastName; } }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+    }
+}
