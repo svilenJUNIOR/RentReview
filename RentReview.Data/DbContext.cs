885a6f746a04bb665ece11918106b5fd127bd09d
svilen iordanov <svilqkaiordanov@gmail.com>
2023-06-21 15:53:21 +0300

    Db context

---
+++ RentReview.Data/DbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RentReview.Data.Models;
+
+namespace RentReview.Data
+{
+    internal class DbContext : IdentityDbContext
+    {
+        public DbContext() { }
+
+        public DbContext(DbContextOptions<DbContext> options) : base(options) { }
+
+        public DbSet<Property> Properties { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tenant> Tenants { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer("Server=;Integrated Security=true;Database=RentReview");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Review>()
+           .HasKey(x => new { x.PropertyId, x.TenantId });
+        }
+    }
+}
