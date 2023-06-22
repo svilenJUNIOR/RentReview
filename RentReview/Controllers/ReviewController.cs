2f84f2227d9e427bb7c8ba70be9bedb06892ddfd
svilen iordanov <svilqkaiordanov@gmail.com>
2023-06-22 14:01:21 +0300

    viewReview page

---
+++ RentReview/Controllers/ReviewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentReview.Controllers
+{
+    public class ReviewController : Controller
+    {
+        public IActionResult ViewReview()
+        {
+            return View();
+        }
+    }
+}
