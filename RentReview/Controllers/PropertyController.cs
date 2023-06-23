b358d5ded561ce815996d08102c23a312cbc6ab1
svilen iordanov <svilqkaiordanov@gmail.com>
2023-06-23 11:58:09 +0300

    All properties page

---
+++ RentReview/Controllers/PropertyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentReview.Controllers
+{
+    public class PropertyController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
