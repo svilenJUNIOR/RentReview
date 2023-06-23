039d79ed1f920ae70b4ffad1a9ec403c66a583a7
svilen iordanov <svilqkaiordanov@gmail.com>
2023-06-23 15:23:01 +0300

    Repo part 1

---
+++ RentReviewRepository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace RentReviewRepository
+{
+    public interface IRepository
+    {
+        Task SaveChangesAsync();
+        Task AddAsync<T>(T newItem) where T : class;
+        Task RemoveAsync<T>(T Item) where T : class;
+
+        ICollection<T> GettAll<T>() where T : class;
+
+        IdentityUser FindUserByEmail(string email);
+        IdentityUser FindUserById(string Id);
+        IdentityRole FindRoleById(string Id);
+
+        T FindById<T>(string Id) where T : class;
+    }
+}
---
+++ RentReviewRepository/Repository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using RentReview.Data;
+
+namespace RentReviewRepository
+{
+    public class Repository : IRepository
+    {
+        private readonly RentDbContext context;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public Repository(RoleManager<IdentityRole> roleManager, RentDbContext context)
+        {
+            this.roleManager = roleManager;
+            this.context = context;
+        }
+
+        public async Task AddAsync<T>(T newItem) where T : class
+        {
+            await context.Set<T>().AddAsync(newItem);
+            await this.SaveChangesAsync();
+        }
+
+        public T FindById<T>(string Id) where T : class
+        {
+            DbSet<T> table = context.Set<T>();
+            return table.Find(Id);
+        } 
+
+        public async IdentityRole FindRoleById(string Id)
+         => await this.roleManager.FindByIdAsync(Id);
+
+        public IdentityUser FindUserByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IdentityUser FindUserById(string Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<T> GettAll<T>() where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync<T>(T Item) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SaveChangesAsync()
+            => await this.context.SaveChangesAsync();
+    }
+}
