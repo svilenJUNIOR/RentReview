070009ce298a4fd234c4fab658724abb56c1f90d
svilen iordanov <svilqkaiordanov@gmail.com>
2023-07-05 11:31:37 +0300

    Review contoller views test return view

---
+++ RentReview.Test/Controllers/Review/ReturnViewTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentReview.Controllers;
+
+namespace RentReview.Test.Controllers.Review
+{
+    public class ReturnViewTest
+    {
+        [Fact]
+        public void AllActionReturnAllView()
+        {
+            var controller = new ReviewController();
+
+            var action = controller.All();
+
+            Assert.IsType<ViewResult>(action);
+        }
+
+        [Fact]
+        public void AddActionReturnAllView()
+        {
+            var controller = new ReviewController();
+
+            var action = controller.Add();
+
+            Assert.IsType<ViewResult>(action);
+        }
+
+        [Fact]
+        public void ViewReviewActionReturnAllView()
+        {
+            var controller = new ReviewController();
+
+            var action = controller.ViewReview();
+
+            Assert.IsType<ViewResult>(action);
+        }
+    }
+}
