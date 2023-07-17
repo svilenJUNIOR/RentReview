bebb2f9e936f68e642e00af6008d0e630b84e51d
svilen iordanov <svilqkaiordanov@gmail.com>
2023-07-17 11:14:16 +0300

    Register / login out of asp area

---
+++ RentReview/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentReview.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Register()
+        => View();
+
+        public IActionResult Login()
+        => View();
+    }
+}
