af6f449a1d1c7ec6d2f7ec5ccfcd6369e7988d7c
svilen iordanov <svilqkaiordanov@gmail.com>
2023-10-03 14:56:52 +0300

    My profile method returns list of own properties

--- RentReview.Test/UserControllerTest.cs
+++ RentReview.Test/UserControllerTest.cs
@@ -2,6 +2,11 @@
 using Xunit;
 using FakeItEasy;
 using Microsoft.AspNetCore.Mvc;
+using RentReview.Services.User;
+using Microsoft.AspNetCore.Identity;
+using RentReview.Models.ViewModels.Property;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
 
 namespace RentReview.Test
 {
@@ -23,6 +28,38 @@ namespace RentReview.Test
 
             var result = userController.Login();
             Assert.IsType<ViewResult>(result);
+        }
+
+        [Theory]
+        [InlineData("22db8ef9-8c04-466a-be5c-1892b970765d")]
+        public void ProfileMethodReturnListOfUsersProperties(string userId)
+        {
+            var fakeUserService = A.Fake<IUserService>();
+            var fakeUserManager = A.Fake<UserManager<IdentityUser>>();
+
+            var userController = new UserController(fakeUserService, null, fakeUserManager)
+            {
+                ControllerContext = new ControllerContext
+                {
+                    HttpContext = new DefaultHttpContext
+                    {
+                        User = new System.Security.Claims.ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                        {
+                            new Claim(ClaimTypes.Name, "Test")
+                        })),
+                    }
+                },
+            };
+
+            var testUser = new IdentityUser() { Id = userId };
+
+            A.CallTo(() => fakeUserService.LoadMyData(testUser))
+                .Returns(new List<ViewPropertyViewModel>(1));
+
+            var result = userController.Profile();
+            var viewResult = Assert.IsType<ViewResult>(result.Result);
+
+            Assert.IsAssignableFrom<ICollection<ViewPropertyViewModel>>(viewResult.ViewData.Model);
         }
     }
 }
