5cd7a0f7f349902eb458ee9a50bba2bb5af09908
svilen iordanov <svilqkaiordanov@gmail.com>
2023-10-03 10:51:11 +0300

    Edit view in review controller returns view with correct model tested

--- RentReview.Test/ReviewControllerTest.cs
+++ RentReview.Test/ReviewControllerTest.cs
@@ -28,13 +28,27 @@ namespace RentReview.Test
         [Fact]
         public void AddViewShouldReturnView()
         {
-            var fakeReviewService = A.Fake<IReviewService>();
-
-            var reviewController = new ReviewController(fakeReviewService, null);
-
+            var reviewController = new ReviewController(null, null);
             var result = reviewController.Add();
 
             Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void EditViewReturnViewWitchCorrectReview()
+        {
+            var Id = "07c6134c-1a84-4b89-b6bf-60ce04de0ecd";
+
+            var fakeReviewService = A.Fake<IReviewService>();
+            var reviewController = new ReviewController(fakeReviewService, null);
+
+            A.CallTo(() => fakeReviewService.ViewFullReview(Id))
+                .Returns(new ViewFullReviewViewModel());
+
+            var result = reviewController.Edit(Id);
+            var viewModel = Assert.IsType<ViewResult>(result);
+
+            Assert.IsType<ViewFullReviewViewModel>(viewModel.Model);
         }
     }
 }
