a9313a90b974319197fd223260878fa931693bb7
svilen iordanov <svilqkaiordanov@gmail.com>
2023-10-04 11:28:07 +0300

    Up to date

--- RentReview.Data/Migrations/20231004082709_Initial.Designer.cs
+++ RentReview.Data/Migrations/20231004082709_Initial.Designer.cs
@@ -12,7 +12,7 @@ using RentReview.Data;
 namespace RentReview.Data.Migrations
 {
     [DbContext(typeof(RentDbContext))]
-    [Migration("20231004081555_Initial")]
+    [Migration("20231004082709_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
