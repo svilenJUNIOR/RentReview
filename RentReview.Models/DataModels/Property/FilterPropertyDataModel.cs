128717dbe19d989128914bc9435ecce75a6858cc
svilen iordanov <svilqkaiordanov@gmail.com>
2023-10-05 09:41:38 +0300

    Filter prop data model

---
+++ RentReview.Models/DataModels/Property/FilterPropertyDataModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RentReview.Models.DataModels.Property
+{
+    public class FilterPropertyDataModel
+    {
+        public int MinPrice { get; set; }
+        public int MaxPrice { get; set; }
+        public ICollection<string> Filters { get; set; } = new HashSet<string>();
+    }
+}
