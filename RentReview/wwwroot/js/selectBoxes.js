eb890a9b54ee82c673ac131bb93d943ddf296e81
svilen iordanov <svilqkaiordanov@gmail.com>
2023-10-06 10:15:15 +0300

    Drop down selext menu

---
+++ RentReview/wwwroot/js/selectBoxes.js
@@ -0,0 +1,27 @@
+var show = true;
+
+function showCheckboxes() {
+    var checkboxes =
+        document.getElementById("checkBoxes");
+
+    if (show) {
+        checkboxes.style.display = "block";
+        show = false;
+    } else {
+        checkboxes.style.display = "none";
+        show = true;
+    }
+}
+
+function showCheckboxes2() {
+    var checkboxes =
+        document.getElementById("checkBoxes2");
+
+    if (show) {
+        checkboxes.style.display = "block";
+        show = false;
+    } else {
+        checkboxes.style.display = "none";
+        show = true;
+    }
+}
